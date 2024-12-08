c413e2fd1d0e8f49603245755cbd7c8749866ad0
blanc091 <mircea.albu91@gmail.com>
2024-12-08 22:30:16 +0200

    Just for test.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,7 +9,7 @@ namespace _200SXProject___MAUI
         public MainPage()
         {
             InitializeComponent();
-            WebView.Source = "https://www.200sxproject.com";
+            WebView.Source = "https://www.200sxproject.com"; // adding this just for test GIT
             ConfigureWebView();
         }
         private void ConfigureWebView()
